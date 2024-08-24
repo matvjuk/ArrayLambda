a0a88f0f36b04fc1345d7e3128ed1a6d451fbc0a
Matvjuk12 <144027748+matvjuk@users.noreply.github.com>
2024-08-24 15:07:14 +0300

    Update LambdaList.h

--- src/MSVC/LambdaList.h
+++ src/MSVC/LambdaList.h
@@ -1,4 +1,15 @@
 #include <string>
-#include <vector>
 #include <experimental/any>
+#include <unordered_map>
+
+class LambdaList
+{
+public:
+    template<typename T>
+    void add(std::string name, T lambda);
+    void remove(std::string name);
+    void* find(std::string name);
+private:
+   std::unordered_map<std::string,std::experimental::any> list;
+};
 
